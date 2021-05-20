0ae59001862a4840501b6073f0ae639532ffd20f
TonyJem <a.jemanov@gmail.com>
2021-05-20 16:08:11 +0300

    Create initial project files and test with "Hello world" print

---
+++ FizzBuzz/FizzBuzz/ViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        mainFunction()
+    }
+}
+
+private extension ViewController {
+    func mainFunction() {
+        print("🟢 Hello world!")
+    }
+    
+}
